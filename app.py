f01d056e586f5fd79e06cdeb5fc220c09855285a
petrokvitka <petrokvitka@gmail.com>
2020-05-27 01:57:13 +0200

    adding image as background

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ import pandas as pd
 import numpy as np
 import sys
 
+from PIL import Image, ImageTk
+
 model = load_model('./new_test/sound_anomality_detection.h5')
+WIDTH, HEIGTH = 550, 1000
 
 def predict_file(fname):
     print("predicting ", fname)
@@ -20,13 +23,23 @@ def browse_file():
 
 def ui_main():
     root = tk.Tk()
-    root.wm_title("Browser")
-    bro_button = tk.Button(master = root, text = "Browse", width = 80, height = 25, command = browse_file)
-    bro_button.pack(side = tk.LEFT, padx = 2, pady = 2)
+    root.wm_title("AMULET")
 
+    canvas = tk.Canvas(root, bg = "white", height = HEIGTH, width = WIDTH)
+    canvas.pack(expand = True) #, fill = "both")
+
+    background_image = ImageTk.PhotoImage(Image.open("amulet_background_handy_logo.png").resize((WIDTH, HEIGTH), Image.ANTIALIAS))
+    canvas.background = background_image #keep a reference in case this code is put in a function
+    bg = canvas.create_image(0, 0, anchor = tk.NW, image = background_image)
+
+    bro_button = tk.Button(master = root, text = "Browse", command = browse_file) # width = 80, height = 25,
+    #bro_button.pack(side = tk.LEFT, padx = 2, pady = 2, expand = True)
+    bro_button_window = canvas.create_window(10, 10, anchor = tk.NW, window = bro_button)
+    """
     submit_button_image = PhotoImage(file = r"./submit_button.png")
     submit_button = tk.Button(master = root, image = submit_button_image)
-    submit_button.pack()
+    submit_button.pack(expand = True)
+    """
 
     tk.mainloop()
 
