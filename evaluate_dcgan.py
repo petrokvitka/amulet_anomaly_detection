6c9d52b6adfa49c8eaffdbd559ee1be1643381f0
Anastasiia <42555602+petrokvitka@users.noreply.github.com>
2020-10-08 19:00:03 +0200

    updating and documentation for the evaluate_dcgan.py

--- evaluate_dcgan.py
+++ evaluate_dcgan.py
@@ -1,60 +1,133 @@
 # deactivate all conda environments,
 # use the default base conda environment
+
+"""
+This script calculates the anomaly threshold after the DCGAN training,
+through comparison of the real MFCCs from the dataset and the generated
+MFCCs after the training.
+Finally, this script loads files from the test dataset and creates a
+ROC-curve to find a range of optimal thresholds.
+"""
+
 from skimage import io, color
 import pandas as pd
 import numpy as np
+import os
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
 
 def convert_to_gray(img_path):
+    """
+    This function converts the color image to black-white.
+    :param img_path: the path to the color image
+    :returns: a black-white image as a 2D array
+    """
     color_img = io.imread(img_path)
     color_gray = color.rgb2gray(color_img)
     return color_gray
 
-train_real_grey = convert_to_gray("../../mfccs/img/1200_200_0.jpg")
-train_generated_grey = convert_to_gray("./dcgan_mfccs_output/samples_745_0.png")
+train_real_gray = convert_to_gray("../../mfccs/img/1200_200_0.jpg")
+train_generated_gray = convert_to_gray("./dcgan_mfccs_output/samples_745_0.png")
 
-# check the shape of grey images
-print(train_generated_grey.shape) #(20, 44)
+# check the shape of gray images
+print(train_generated_gray.shape) #(20, 44)
 
-differences = np.abs(train_real_grey.flatten() - train_generated_grey.flatten())
+# calculate the anomaly threshold as a maximum of differences
+differences = np.abs(train_real_gray.flatten() - train_generated_gray.flatten())
 threshold = np.max(differences)
 print(threshold) #0.17
 
+def read_files(directory):
+    """
+    This function reads the MFCCs in JPG format from the given directory.
+    :param directory: the directory to read the files from
+    :returns: a list of path to the MFCC files
+    """
+    files = []
+    for f in os.listdir(directory):
+        if f.endswith(".jpg"):
+            files.append(os.path.join(directory, os.path.basename(f)))
 
-# now test this threshold with the test data
-bad_real_color = io.imread("../../mfccs/test/bad_22050.jpg")
-bad_real_grey = color.rgb2gray(bad_real_color)
+    return files
 
-defect_real_color = io.imread("../../mfccs/test/defect_22050.jpg")
-defect_real_grey = color.rgb2gray(defect_real_color)
+# set the directories where we read the MFCCs for testing
+good_real = read_files("../../mfccs/test/good")
+bad_real = read_files("../../mfccs/test/bad")
+defect_real = read_files("../../mfccs/test/defect")
 
-good_real_color = io.imread("../../mfccs/test/good_22050.jpg")
-good_real_grey = color.rgb2gray(good_real_color)
+good_generated = read_files("./dcgan_mfccs_output/test/good")
+bad_generated = read_files("./dcgan_mfccs_output/test/bad")
+defect_generated = read_files("./dcgan_mfccs_output/test/defect")
+
+# create a range of thresholds in regard to the calculated threshold
+thresholds = [threshold - 0.1, threshold - 0.05, threshold, threshold + 0.05, threshold + 0.1, threshold + 0.15, threshold + 0.2, threshold + 0.25]
+
+# calculate the values of TPR and FPR
+true_positives = []
+false_positives = []
+true_negatives = []
+false_negatives = []
+tprs = []
+fprs = []
+
+for threshold in thresholds:
+    print(threshold)
+    false_negative = 0
+    true_positive = 0
+    for i in range(min(len(good_real), len(good_generated))):
+        real = convert_to_gray(good_real[i])
+        generated = convert_to_gray(good_generated[i])
+        differences = np.abs(real.flatten() - generated.flatten())
+        if max(differences) > threshold:
+            print("false negative")
+            false_negative += 1
+        else:
+            print("true positive")
+            true_positive += 1
+
+    false_positive = 0
+    true_negative = 0
+    for i in range(min(len(defect_real), len(defect_generated))):
+        real = convert_to_gray(defect_real[i])
+        generated = convert_to_gray(defect_generated[i])
+        differences = np.abs(real.flatten() - generated.flatten())
+        if max(differences) > threshold:
+            print("true negative")
+            true_negative += 1
+        else:
+            print("false positive")
+            false_positive += 1
+
+    tpr = true_positive / (true_positive + false_negative)
+    fpr = false_positive / (true_negative + false_positive)
+    true_positives.append(true_positive)
+    true_negatives.append(true_negative)
+    false_positives.append(false_positive)
+    false_negatives.append(false_negative)
+    tprs.append(tpr)
+    fprs.append(fpr)
+
+# check the values of TPR and FPR for different thresholds
+print(tprs)
+print(fprs)
+
+# now plot the ROC-curve
+fig, ax = plt.subplots(figsize = (26, 13), dpi = 180, facecolor = 'w', edgecolor = 'k')
+
+roc = ax.plot(fprs, tprs, marker = 'o', label = "Trained for 750 epochs DCGAN model")
+
+d = ax.plot(np.arange(0, 1.1, 0.1), np.arange(0, 1.1, 0.1))
+d[0].set_linestyle('dashed')
+
+plt.text(0, 0, "Thresholds {:.3f} to {:.3f}".format(thresholds[0], thresholds[2]))
+plt.text(0, 0.95, "Thresholds {:.3f} to {:.3f}".format(thresholds[3], thresholds[5]))
+plt.text(0.55, 0.95, "Threshold {:.3f}".format(thresholds[6]))
+plt.text(0.85, 0.95, "Threshold {:.3f}".format(thresholds[7]))
 
 
-
-bad_generated_grey = convert_to_gray("./dcgan_mfccs_output/test/bad/samples_0_1.jpg")
-good_generated_grey = convert_to_gray("./dcgan_mfccs_output/test/good/samples_0_1.jpg")
-defect_generated_grey = convert_to_gray("./dcgan_mfccs_output/test/defect/samples_0_1.jpg")
-
-
-bad_differences = np.abs(bad_real_grey.flatten() - bad_generated_grey.flatten())
-good_differences = np.abs(good_real_grey.flatten() - good_generated_grey.flatten())
-defect_differences = np.abs(defect_real_grey.flatten() - defect_generated_grey.flatten())
-
-"""
-if max(bad_differences) > threshold:
-    #print("defect")
-    print("true negative")
-else:
-    print("false positive")
-"""
-
-if max(good_differences) > threshold:
-    print("true positive")
-else:
-    print("false negative")
-
-if max(defect_differences) > threshold:
-    print("true negative")
-else:
-    print("false positive")
+ax.set_ylabel("TPR")
+ax.set_xlabel("FPR")
+ax.set_title("ROC curve for thresholds from {} to {}.".format(round(min(thresholds), 3), round(max(thresholds), 3)))
+ax.legend(loc = 'lower right')
+plt.show()
